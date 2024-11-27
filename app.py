40b59c24b8b42fd8ed1d4510d60c9296a662745d
messex04 messex04 <messina.daniele.2004@gmail.com>
2024-11-27 09:24:31 +0000

    fase 1

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
+
 from flask import Flask
-6.
-7. #inizializza l'app Flask
-8. app = Flask(__name__)
-9.
-10.#rotta principale
-11.@app.route('/')
-12.def home():
-13. return "Per ora funziona tutto"
-14.
-15.#avvio dell'app Flask
-16.if __name__ == '__main__':
-17. app.run(debug=True)
+
+
+ #inizializza l'app Flask
+ app = Flask(__name__)
+
+#rotta principale
+
+@app.route('/')
+def home():
+ return "Per ora funziona tutto"
+
+#avvio dell'app Flask
+
+if __name__ == '__main__':
+ app.run(debug=True)
